6c44e6694d2a33490415cd80be8dc98edf3bcb15
alex-Linux <vootehdoo@gmail.com>
2024-03-07 11:12:20 +0100

    Add SASS variables, partials and mix-ins

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import "./assets/styles/app.css";
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Hello</h1>
+      <br />
+      <h2>Refresher on how to import SASS found @:</h2>
+      <p>/client/src/assets/styles/app.scss</p>
+      <br />
+      <h2>Refresher how to export a variable or mixin found @:</h2>
+      <p>/client/src/assets/_index.scss</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
