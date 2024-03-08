4d289e790ab11876a55a88288cec2fdbe9101dff
DanteBoute <danteboute1@outlook.com>
2024-03-08 16:49:53 +0100

    Make nav bar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,16 @@
 import "./assets/styles/app.css";
+import Navbar from "./components/Navbar";
 function App() {
   return (
     <>
-      <h1>Hello</h1>
+    <Navbar />
+      {/* <h1>Hello</h1>
       <br />
       <h2>Refresher on how to import SASS found @:</h2>
       <p>/client/src/assets/styles/app.scss</p>
       <br />
       <h2>Refresher how to export a variable or mixin found @:</h2>
-      <p>/client/src/assets/_index.scss</p>
+      <p>/client/src/assets/_index.scss</p> */}
     </>
   );
 }
