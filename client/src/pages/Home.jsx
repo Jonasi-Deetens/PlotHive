d989c2b33c86554c83ae694a941fcec45834102e
DanteBoute <danteboute1@outlook.com>
2024-03-11 15:48:05 +0100

    add cards to homepage

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -11,6 +11,24 @@ const Home = () => {
       <button className='home-header-button'><Link to="/Write" className='home-header-button'> Keep writing </Link></button>
       <button className='home-header-button'><Link to="/Explore" className='home-header-button'>Explore</Link></button>
     </div>
+    <div className="card-container">
+  <div className="card">
+    <i className="fas fa-icon"></i>
+    <p>Card 1</p>
+  </div>
+  <div className="card">
+    <i className="fas fa-icon"></i>
+    <p>Card 2</p>
+  </div>
+  <div className="card">
+    <i className="fas fa-icon"></i>
+    <p>Card 3</p>
+  </div>
+  <div className="card">
+    <i className="fas fa-icon"></i>
+    <p>Card 4</p>
+  </div>
+</div>
     </main>
     </>
   )
