975e7c7a02e4bf2bc81528af3fb723dabc90ecd7
DanteBoute <danteboute1@outlook.com>
2024-03-12 17:08:17 +0100

    Finish homepage without responsiveness

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,58 +1,81 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 import "../assets/styles/pages/Home/home.css";
 
 const Home = () => {
   return (
-    <main className='home-page'>
-      <div className='home-page-navbar'>
-      <img src="src/assets/Logo/logo-home.svg" alt="Large-Logo" />
-      <button className='home-page-login'><Link to="/Login">Login</Link></button>
+    <main className="home-page">
+      <header className="home-page-header">
+        <div className="home-page-navbar">
+          <Link to="/">
+            {" "}
+            {/* Link to the homepage */}
+            <img src="src/assets/Logo/logo-home.svg" alt="Large-Logo" />
+          </Link>
+          <button className="home-page-login">
+            <Link to="/Login">Login</Link>
+          </button>
+        </div>
+        <div className="main-header-container">
+          <h1 className="main-header">
+            {" "}
+            &quot;Jonasi stepped out of the elevator with blood all over his
+            face, ... &quot;
+          </h1>
+        </div>
+        <div className="home-button-container">
+          <button className="home-header-button">
+            <Link to="/Write" className="home-link">
+              Keep writing
+            </Link>
+          </button>
+          <button className="home-header-button">
+            <Link to="/Explore" className="home-link">
+              Explore
+            </Link>
+          </button>
+        </div>
+      </header>
+      <div className="card-container">
+        <Link to="/About" className="card">
+          <img src="src/assets/Logo/logo-navbar.svg" alt="icon1" />
+          <p>Write stories as a community</p>
+        </Link>
+        <Link to="/About" className="card">
+          <img src="src/assets/Logo/logo-navbar.svg" alt="icon2" />
+          <p>Upvote the part that would fit the story best</p>
+        </Link>
+        <Link to="/About" className="card">
+          <img src="src/assets/Logo/logo-navbar.svg" alt="icon3" />
+          <p>Earn by contributing</p>
+        </Link>
+        <Link to="/About" className="card">
+          <img src="src/assets/Logo/logo-navbar.svg" alt="icon4" />
+          <p>Earn badges</p>
+        </Link>
       </div>
-      <h1 className='main-header'> &quot;Jonasi stepped out of the elevator with blood all over his face, ... &quot;</h1>
-      <div className='button-container'>
-        <button className='home-header-button'><Link to="/Write" className='home-link'>Keep writing</Link></button>
-        <button className='home-header-button'><Link to="/Explore" className='home-link'>Explore</Link></button>
-      </div>
-        
-        <div className="card-container">
-          <Link to="/About" className="card">
+      <h1 className="popular-header">Popular right now</h1>
+      <div className="popular-section">
+        <div className="popular-cards">
+          <Link to="/Store" className="popular-card">
             <img src="src/assets/Logo/logo-navbar.svg" alt="icon1" />
-            <p>Write stories as a community</p>
+            <p>Popular story 1</p>
           </Link>
-          <Link to="/About" className="card">
+          <Link to="/Store" className="popular-card">
             <img src="src/assets/Logo/logo-navbar.svg" alt="icon2" />
-            <p>Upvote the part that would fit the story best</p>
+            <p>Popular story 2</p>
           </Link>
-          <Link to="/About" className="card">
+          <Link to="/Store" className="popular-card">
             <img src="src/assets/Logo/logo-navbar.svg" alt="icon3" />
-            <p>Earn by contributing</p>
-          </Link>
-          <Link to="/About" className="card">
-            <img src="src/assets/Logo/logo-navbar.svg" alt="icon4" />
-            <p>Earn badges</p>
+            <p>Popular story 3</p>
           </Link>
         </div>
-        <h1 className='popular-header'>Popular right now</h1>
-        <div className="popular-section">
-          <div className='popular-cards'>
-            <div className="popular-card">
-              <img src="src/assets/Logo/logo-navbar.svg" alt="icon1" />
-              <p>Popular story 1</p>
-            </div>
-            <div className="popular-card">
-              <img src="src/assets/Logo/logo-navbar.svg" alt="icon2" />
-              <p>Popular story 2</p>
-            </div>
-            <div className="popular-card">
-              <img src="src/assets/Logo/logo-navbar.svg" alt="icon3" />
-              <p>Popular story 3</p>
-            </div>
-          </div>
-        </div>
-        <h2 className='section-three'>Welcome to your journey through the magical art of writing.</h2>
+      </div>
+      <h2 className="section-three">
+        Welcome to your journey through the magical art of writing.
+      </h2>
     </main>
   );
-}
+};
 
 export default Home;
