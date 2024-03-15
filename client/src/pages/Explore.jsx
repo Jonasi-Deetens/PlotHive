60c0b921332de9489d131942ef01d4884fd17449
alex-Linux <vootehdoo@gmail.com>
2024-03-15 15:00:36 +0100

    Add basic elements to Expolore page

--- client/src/pages/Explore.jsx
+++ client/src/pages/Explore.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import "../assets/styles/pages/Explore/explore.css";
+import loupe from "../assets/svgs/search.png";
+import BookShowcase from "../components/BookShowcase";
 
 const Explore = () => {
   return (
-    <div>Explore</div>
-  )
-}
+    <>
+      <h1>Explore our titles!</h1>
+      <div className="search-bar">
+        <input
+          type="search"
+          name="search"
+          id="search"
+          placeholder="e.g. Pieter and the little kids..."
+        />
+        <a href="">
+          <img src={loupe} alt="search loupe image" />
+        </a>
+      </div>
+      <BookShowcase category={"search-results"} />
+      <BookShowcase category={"like-this"} />
+    </>
+  );
+};
 
-export default Explore
+export default Explore;
