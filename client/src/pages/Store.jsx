9f171d443ce7cef9fcc9e3a1a2df4480539276ab
DanteBoute <danteboute1@outlook.com>
2024-03-15 12:16:34 +0100

    Remove footer on 404 page

--- client/src/pages/Store.jsx
+++ client/src/pages/Store.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
+import BookShowcase from "../components/BookShowcase";
 
 const Store = () => {
   return (
-    <div>Store</div>
-  )
-}
+    <div>
+      <BookShowcase category={"top"} />
+      <BookShowcase category={"like-this"} />
+    </div>
+  );
+};
 
-export default Store
+export default Store;
