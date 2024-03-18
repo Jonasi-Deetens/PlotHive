060346ded8ae5e7059f7abe5ccca82cf80aabe0a
alex-Linux <vootehdoo@gmail.com>
2024-03-18 15:29:58 +0100

    Fix the Hamburger modal

--- client/src/pages/Explore.jsx
+++ client/src/pages/Explore.jsx
@@ -13,7 +13,7 @@ const Explore = () => {
   };
 
   return (
-    <>
+    <main>
       <h1>Explore our titles!</h1>
       <div className="search-bar">
         <input
@@ -23,13 +23,11 @@ const Explore = () => {
           placeholder="e.g. Pieter and the little kids..."
           onChange={handleSearch}
         />
-        <a href="">
-          <img src={loupe} alt="search loupe image" />
-        </a>
+        <img src={loupe} alt="search loupe image" />
       </div>
       {query && <BookShowcase category={"search-results"} query={query} />}
       <BookShowcase category={"like-this"} />
-    </>
+    </main>
   );
 };
 
