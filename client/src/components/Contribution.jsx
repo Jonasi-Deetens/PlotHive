d993148a4c4ba1760cb941d69215faab82936248
Kilian Boute <kilian.boute@gmail.com>
2024-03-20 16:02:03 +0100

    Add comment section to contributions

--- client/src/components/Contribution.jsx
+++ client/src/components/Contribution.jsx
@@ -1,12 +1,16 @@
+/* eslint-disable react/prop-types */
 import { useContext, useState } from "react";
 import "../assets/styles/components/Contribution/contribution.css";
 import { UserContext } from "../providers/UserContext";
+import CommentSection from "./CommentSection";
 
 const Contribution = ({ contribution, userContribution }) => {
   const { user } = useContext(UserContext);
   const { text, upvoters, user_id } = contribution;
   const username = user_id.username;
-  const [upvoted, setUpvoted] = useState(contribution.upvoters.indexOf(user._id) != -1);
+  const [upvoted, setUpvoted] = useState(
+    contribution.upvoters.indexOf(user._id) != -1
+  );
 
   const upvote = async () => {
     const index = contribution.upvoters.indexOf(user._id);
@@ -17,15 +21,18 @@ const Contribution = ({ contribution, userContribution }) => {
     }
 
     try {
-      const response = await fetch('http://127.0.0.1:5000/api/contributions/' + contribution._id, {
-        method: 'PATCH',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          upvoters: contribution.upvoters
-        })
-      })
+      const response = await fetch(
+        "http://127.0.0.1:5000/api/contributions/" + contribution._id,
+        {
+          method: "PATCH",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            upvoters: contribution.upvoters,
+          }),
+        }
+      );
       if (response.ok) {
         upvoted ? alert("Succesfully unvoted!") : alert("Succesfully upvoted!");
         setUpvoted(!upvoted);
@@ -33,22 +40,30 @@ const Contribution = ({ contribution, userContribution }) => {
     } catch (error) {
       console.log(error.message);
     }
-  }
+  };
 
   return (
     <div className="contribution-component">
-      { !userContribution && 
-      <div className="contribution-wrapper">
-        <button onClick={upvote} className="contribution-button">
-          <img
-            src={upvoted ? "src/assets/svgs/vote-yellow.svg" : "src/assets/svgs/vote.svg"}
-            alt="icon of a arrow pointing up"
-          />
-        </button>
-        <p>{upvoters.length}</p>
-      </div> }
+      {!userContribution && (
+        <div className="contribution-wrapper">
+          <button onClick={upvote} className="contribution-button">
+            <img
+              src={
+                upvoted
+                  ? "src/assets/svgs/vote-yellow.svg"
+                  : "src/assets/svgs/vote.svg"
+              }
+              alt="icon of a arrow pointing up"
+            />
+          </button>
+          <p>{upvoters.length}</p>
+        </div>
+      )}
       <div dangerouslySetInnerHTML={{ __html: text }}></div>
-      <p className="contribution-author">By: {!userContribution ? username : 'You'}</p>
+      <p className="contribution-author">
+        By: {!userContribution ? username : "You"}
+      </p>
+      <CommentSection contribtion={contribution} />
     </div>
   );
 };
