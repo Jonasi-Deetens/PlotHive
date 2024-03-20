46b0c569bb59cf33ad5e26eba85d7729d093815c
alex-Linux <vootehdoo@gmail.com>
2024-03-20 10:57:00 +0100

    Add genre buttons under search bar

--- client/src/pages/Explore.jsx
+++ client/src/pages/Explore.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "../assets/styles/pages/Explore/explore.css";
 import loupe from "../assets/svgs/search.png";
 import BookShowcase from "../components/BookShowcase";
@@ -6,6 +6,8 @@ useState;
 
 const Explore = () => {
   const [query, setQuery] = useState(null);
+  const [genres, setGenres] = useState(null);
+  const [selectedGenre, setSelectedGenre] = useState(null);
   let timeoutId = null;
 
   let handleSearch = ({ currentTarget = {} }) => {
@@ -20,6 +22,36 @@ const Explore = () => {
     }, 300);
   };
 
+  useEffect(() => {
+    const getGenres = async () => {
+      try {
+        const response = await fetch("http://127.0.0.1:5000/api/genres", {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+        if (response.ok) {
+          const data = await response.json();
+          const titles = data.map((genre) => genre.title);
+          setGenres(titles);
+        }
+      } catch (error) {
+        throw new Error(error.message);
+      }
+    };
+
+    getGenres();
+  }, []);
+
+  let handleSelected = (genre) => {
+    if (genre === selectedGenre) {
+      setSelectedGenre(null);
+    } else {
+      setSelectedGenre(genre);
+    }
+  };
+
   return (
     <main>
       <h1 className="explore">Explore our titles!</h1>
@@ -32,6 +64,16 @@ const Explore = () => {
           onChange={handleSearch}
         />
         <img src={loupe} alt="search loupe image" />
+      </div>
+      <div className="search-buttons">
+        <ul className="buttons-list">
+          {genres &&
+            genres.map((genre) => (
+              <button key={genre} onClick={() => handleSelected(genre)}>
+                {genre}
+              </button>
+            ))}
+        </ul>
       </div>
       {query && <BookShowcase category={"search-results"} query={query} />}
       <BookShowcase category={"like-this"} />
