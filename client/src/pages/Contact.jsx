d160f06961c03914c3ebc3328f3e2b1913e45ec7
TheNyxs <jonasideetens@hotmail.com>
2024-03-21 01:24:58 +0100

    Make several pages responsive

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -13,7 +13,6 @@ const Contact = () => {
     <main className="contact-page">
       <div className="contact-wrapper">
         <div className="social-media-container">
-          <h2 className="socials-title">Socials</h2>
           <ul className="socials-list">
             <li>
               <a
@@ -56,7 +55,7 @@ const Contact = () => {
         <div className="contact-container">
           <h2 className="contact-title">Contact Us</h2>
           <div className="errors">
-            <p className="error-message">Email can&apos;t be empty!</p>
+            <p className="error-message"></p>
           </div>
           <form action="">
             <div className="name">
@@ -124,17 +123,17 @@ const Contact = () => {
             </button>
           </form>
         </div>
-      </div>
-      <div className="contact-map">
-        <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2508.9072257920957!2d3.7349657000000005!3d51.0363342!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c371556f52318f%3A0x5f5246c3223b1ba!2sZuiderpoort!5e0!3m2!1sen!2sbe!4v1710236459275!5m2!1sen!2sbe"
-          width="1000"
-          height="600"
-          allowFullScreen=""
-          loading="lazy"
-          referrerPolicy="no-referrer-when-downgrade"
-          title="Google Maps"
-        ></iframe>
+        <div className="contact-map">
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2508.9072257920957!2d3.7349657000000005!3d51.0363342!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c371556f52318f%3A0x5f5246c3223b1ba!2sZuiderpoort!5e0!3m2!1sen!2sbe!4v1710236459275!5m2!1sen!2sbe"
+            width="1000"
+            height="600"
+            allowFullScreen=""
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+            title="Google Maps"
+          ></iframe>
+        </div>
       </div>
     </main>
   );
