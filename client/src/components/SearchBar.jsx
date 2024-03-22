254cf02f6e2e9ff081746010157dfad79e543f78
alex-Linux <vootehdoo@gmail.com>
2024-03-22 11:34:34 +0100

    Make search bar separate component, fix some css

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -1,20 +1,88 @@
+import { useEffect, useState } from "react";
 import "../assets/styles/components/SearchBar/searchbar.css";
+import BookShowcase from "./BookShowcase";
 
-const SearchBar = ({ placeholder, buttonText, onSubmit }) => {
+const SearchBar = ({ placeholder }) => {
+  const [query, setQuery] = useState(null);
+  const [genres, setGenres] = useState(null);
+  const [selectedGenre, setSelectedGenre] = useState(null);
+  let timeoutId = null;
+
+  let handleSearch = ({ currentTarget = {} }) => {
+    const { value } = currentTarget;
+
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+    }
+
+    timeoutId = setTimeout(() => {
+      setQuery(value);
+    }, 300);
+  };
+
+  useEffect(() => {
+    const getGenres = async () => {
+      try {
+        const response = await fetch("http://127.0.0.1:5000/api/genres", {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+        if (response.ok) {
+          const data = await response.json();
+          const titles = data.map((genre) => genre.title);
+          setGenres(titles);
+        }
+      } catch (error) {
+        throw new Error(error.message);
+      }
+    };
+
+    getGenres();
+  }, []);
+
+  let handleSelected = (genre) => {
+    if (genre === selectedGenre) {
+      setSelectedGenre(null);
+    } else {
+      setSelectedGenre(genre);
+    }
+  };
   return (
-    <div className="search-bar">
-      <input
-        type="search"
-        name="search"
-        id="search"
-        placeholder={placeholder}
-      />
-      <div className="button-div">
-        <button className="searchbar-button" onClick={onSubmit}>
-          {buttonText}
-        </button>
+    <>
+      <div className="search-bar">
+        <input
+          type="search"
+          name="search"
+          id="search"
+          placeholder={placeholder}
+          onChange={handleSearch}
+        />
+        <div className="button-div">
+          <button className="searchbar-button" onClick={handleSearch}>
+            <img src="src/assets/svgs/search.png" alt="search-loupe-image" />
+          </button>
+        </div>
       </div>
-    </div>
+      <div className="search-buttons">
+        <ul className="buttons-list">
+          {genres &&
+            genres.map((genre) => (
+              <button key={genre} onClick={() => handleSelected(genre)}>
+                {genre}
+              </button>
+            ))}
+        </ul>
+      </div>
+      {(query || selectedGenre) && (
+        <BookShowcase
+          category={"search-results"}
+          query={query}
+          selectedGenre={selectedGenre}
+        />
+      )}
+    </>
   );
 };
 
--- client/src/pages/Explore.jsx
+++ client/src/pages/Explore.jsx
@@ -2,86 +2,60 @@ import { useEffect, useState } from "react";
 import "../assets/styles/pages/Explore/explore.css";
 import loupe from "../assets/svgs/search.png";
 import BookShowcase from "../components/BookShowcase";
-useState;
+import SearchBar from "../components/SearchBar";
 
 const Explore = () => {
-  const [query, setQuery] = useState(null);
-  const [genres, setGenres] = useState(null);
-  const [selectedGenre, setSelectedGenre] = useState(null);
-  let timeoutId = null;
+  // const [query, setQuery] = useState(null);
+  // const [genres, setGenres] = useState(null);
+  // const [selectedGenre, setSelectedGenre] = useState(null);
+  // let timeoutId = null;
 
-  let handleSearch = ({ currentTarget = {} }) => {
-    const { value } = currentTarget;
+  // let handleSearch = ({ currentTarget = {} }) => {
+  //   const { value } = currentTarget;
 
-    if (timeoutId) {
-      clearTimeout(timeoutId);
-    }
+  //   if (timeoutId) {
+  //     clearTimeout(timeoutId);
+  //   }
 
-    timeoutId = setTimeout(() => {
-      setQuery(value);
-    }, 300);
-  };
+  //   timeoutId = setTimeout(() => {
+  //     setQuery(value);
+  //   }, 300);
+  // };
 
-  useEffect(() => {
-    const getGenres = async () => {
-      try {
-        const response = await fetch("http://127.0.0.1:5000/api/genres", {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-          },
-        });
-        if (response.ok) {
-          const data = await response.json();
-          const titles = data.map((genre) => genre.title);
-          setGenres(titles);
-        }
-      } catch (error) {
-        throw new Error(error.message);
-      }
-    };
+  // useEffect(() => {
+  //   const getGenres = async () => {
+  //     try {
+  //       const response = await fetch("http://127.0.0.1:5000/api/genres", {
+  //         method: "GET",
+  //         headers: {
+  //           "Content-Type": "application/json",
+  //         },
+  //       });
+  //       if (response.ok) {
+  //         const data = await response.json();
+  //         const titles = data.map((genre) => genre.title);
+  //         setGenres(titles);
+  //       }
+  //     } catch (error) {
+  //       throw new Error(error.message);
+  //     }
+  //   };
 
-    getGenres();
-  }, []);
+  //   getGenres();
+  // }, []);
 
-  let handleSelected = (genre) => {
-    if (genre === selectedGenre) {
-      setSelectedGenre(null);
-    } else {
-      setSelectedGenre(genre);
-    }
-  };
+  // let handleSelected = (genre) => {
+  //   if (genre === selectedGenre) {
+  //     setSelectedGenre(null);
+  //   } else {
+  //     setSelectedGenre(genre);
+  //   }
+  // };
 
   return (
     <main className="explore-page">
       <h1 className="explore">Explore our titles!</h1>
-      <div className="search-bar">
-        <input
-          type="search"
-          name="search"
-          id="search"
-          placeholder="e.g. Pieter and the little kids..."
-          onChange={handleSearch}
-        />
-        <img src={loupe} alt="search loupe image" />
-      </div>
-      <div className="search-buttons">
-        <ul className="buttons-list">
-          {genres &&
-            genres.map((genre) => (
-              <button key={genre} onClick={() => handleSelected(genre)}>
-                {genre}
-              </button>
-            ))}
-        </ul>
-      </div>
-      {(query || selectedGenre) && (
-        <BookShowcase
-          category={"search-results"}
-          query={query}
-          selectedGenre={selectedGenre}
-        />
-      )}
+      <SearchBar placeholder={"Pieter and the little kids..."} />
       <BookShowcase category={"like-this"} />
     </main>
   );
